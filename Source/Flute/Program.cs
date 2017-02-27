389daa2eb138df692236feae071eaa4fa1139cc4
Aref <MasterAref@users.noreply.github.com>
2017-02-28 02:41:01 +0330

    Add interface signs
    Added interfaces are:
    * ISourceDownloading
    * ISourceSaving
    * ILinkMagnetor
    * IUserConfiguration
    * IPropertiesDetector

--- Source/Flute/Program.cs
+++ Source/Flute/Program.cs
@@ -10,6 +10,48 @@ namespace Flute
    {
       static void Main(string[] args)
       {
+
+         Console.ReadLine();
       }
+   }
+
+   /// <summary>
+   /// Downloading sources on internet.
+   /// </summary>
+   interface ISourceDownloading
+   {
+      //TODO: Implement
+   }
+
+   /// <summary>
+   /// Saving downloaded source as a file.
+   /// </summary>
+   interface ISourceSaving
+   {
+      //TODO: Implement
+   }
+
+   /// <summary>
+   /// Get link copied into clipboard.
+   /// </summary>
+   interface ILinkMagnetor
+   {
+      //TODO: Implement 
+   }
+
+   /// <summary>
+   /// Handling *.cfg file.
+   /// </summary>
+   interface IUserConfiguration
+   {
+      //TODO: Implement
+   }
+
+   /// <summary>
+   /// Detecting properties related to object need to be download.
+   /// </summary>
+   interface IPropertiesDetector
+   {
+      //TODO: Implement
    }
 }
